e07dce34a707a688ca71b51d3475e1779b9cf323
Julien Le Goff <jlegoff@newrelic.com>
2022-10-03 12:10:42 +0200

    Do not commit protos to the repository

--- proto/build.gradle.kts
+++ proto/build.gradle.kts
@@ -1,24 +1,31 @@
-description = "OpenTelemetry Opamp Protobufs"
+import com.google.protobuf.gradle.*
 
 plugins {
-    `java-library`
-}
-
-tasks {
-    withType<JavaCompile>().configureEach {
-        with(options) {
-            release.set(8)
-
-            //disable deprecation warnings for the protobuf module
-            compilerArgs.add("-Xlint:-deprecation")
-
-            encoding = "UTF-8"
-        }
-    }
+  id("otel.java-conventions")
+  id("com.google.protobuf")
 }
 
 val protobufVersion = "3.19.4"
 
 dependencies {
-    implementation("com.google.protobuf:protobuf-java:$protobufVersion")
+  api("com.google.protobuf:protobuf-java:$protobufVersion")
+}
+
+protobuf {
+  protoc {
+    artifact = "com.google.protobuf:protoc:$protobufVersion"
+  }
+}
+
+sourceSets {
+  main {
+    proto {
+      srcDir("../opamp-spec/proto")
+    }
+  }
+}
+
+repositories {
+  mavenCentral()
+  mavenLocal()
 }
