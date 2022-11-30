c671a866544aff1490fd5a8d91cbc831274743fc
Vibhavari Mandal <vmandal@sumologic.com>
2022-11-30 21:56:49 +0530

    Basic CI Setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,33 @@
 plugins {
   id("otel.java-conventions")
+  id("java-library")
+  id("maven-publish")
 }
 
 repositories {
   mavenCentral()
   mavenLocal()
+}
+
+dependencies {
+  implementation(project(":proto"))
+}
+
+group = "io.opentelemetry"
+version = "1.0.0"
+
+publishing {
+  publications.create<MavenPublication>("lib") {
+    from(components["java"])
+  }
+  repositories.maven("/tmp/opamp-java")
+}
+
+tasks.jar {
+  from(sourceSets.main.get().output)
+  dependsOn(configurations.runtimeClasspath)
+  from({
+    configurations.runtimeClasspath.get().filter { it.name.equals("proto.jar") }.map { zipTree(it) }
+  })
+  duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
