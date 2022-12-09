b7b56f6848549c28eafb207562bb150da3051e04
VIBHAVARI MANDAL <83277109+VibhavariMandal-sumo@users.noreply.github.com>
2022-12-09 22:00:47 +0530

    Create version.gradle.kts

---
+++ version.gradle.kts
@@ -0,0 +1,13 @@
+val snapshot = true
+
+allprojects {
+  var ver = "1.21.0"
+  val release = findProperty("otel.release")
+  if (release != null) {
+    ver += "-" + release
+  }
+  if (snapshot) {
+    ver += "-SNAPSHOT"
+  }
+  version = ver
+}
